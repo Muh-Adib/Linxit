c75794488d3708105f4bc78d925e7e2911861743
Muh-Adib <muhammad.17@students.amikom.ac.id>
2024-06-07 19:24:15 +0000

    api setting sementara

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Requests\Auth\LoginRequest;
 
 Route::middleware(['auth:sanctum', 'verified', 'jwt.verify'])->group(function () { 
     // Route yang hanya bisa diakses oleh user yang terautentikasi dengan JWT
+    Route::post('/api/login', [AuthController::class, 'login']);
 });
