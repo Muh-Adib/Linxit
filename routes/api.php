f499106b8e76f61263187ef1850bb927b7b8491a
Muh-Adib <muhammad.17@students.amikom.ac.id>
2024-06-07 19:39:44 +0000

    add axios & edit api.php

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Requests\Auth\LoginRequest;
 
-Route::middleware(['auth:sanctum', 'verified', 'jwt.verify'])->group(function () { 
-    // Route yang hanya bisa diakses oleh user yang terautentikasi dengan JWT
-    Route::post('/api/login', [AuthController::class, 'login']);
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('verified')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/api/login', [AuthController::class, 'login'])->middleware('guest');
+Route::post('/api/logout', [AuthController::class, 'logout'])->middleware('jwt.verify');
+
+Route::middleware(['jwt.verify'])->group(function () {
+    //  Tambahkan route API yang dilindungi JWT di sini
 });
